48366ff97338b613b8dff5798a8d40e11a8b4818
Rohit Kardani <rk.developer.work@gmail.com>
2026-01-31 18:15:54 +0530

    PWA implementation

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'clothyfly-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './product_list.html',
+    './product_detail.html',
+    './Clothyfly-dark.svg',
+    './Clothyfly-light.svg',
+    './manifest.json'
+];
+
+// Install Event - Cache Core Assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Activate Event - Clean up old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event - Serve from Cache, Fallback to Network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Return cached response if found
+            if (response) {
+                return response;
+            }
+            // Otherwise fetch from network
+            return fetch(event.request);
+        })
+    );
+});
